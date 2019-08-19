814afb6dd6fa29d98daaa0b01072b5816a9cb0e0
Aney <nanyi1986@126.com>
2019-08-19 17:18:05 +0800

    Add files via upload

---
+++ Frameworks/BaiduMobAdSDK.framework/Headers/BaiduMobAdPrerollDelegate.h
@@ -0,0 +1,44 @@
+//
+//  BaiduMobAdPrerollDelegate.h
+//  BaiduMobAdSdk
+//
+//  Created by lishan04 on 15-6-8.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "BaiduMobAdCommonConfig.h"
+
+@class BaiduMobAdPreroll;
+
+@protocol BaiduMobAdPrerollDelegate <NSObject>
+
+@optional
+
+/**
+ *  加载成功
+@param  adMaterialType  BaiduMobMaterialType
+ */
+- (void)prerollAdloadSuccess:(BaiduMobAdPreroll *)preroll withAdMaterialType:(NSString *)adMaterialType;
+
+/**
+ *  展示成功
+ */
+- (void)prerollAdDidStart:(BaiduMobAdPreroll *)preroll;
+
+/**
+ *  广告展示失败
+ */
+- (void)prerollAdDidFailed:(BaiduMobAdPreroll *)preroll withError:(BaiduMobFailReason) reason;
+
+/**
+ *  广告展示结束
+ */
+- (void)prerollAdDidFinish:(BaiduMobAdPreroll *)preroll;
+
+/**
+ *  广告点击
+ */
+- (void)prerollAdDidClicked:(BaiduMobAdPreroll *)preroll;
+
+@end
